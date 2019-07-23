abd8f5f742f4ae256580028d7850622f29134ac3
Marcos Crispino <mcrispino@genexus.com>
2019-07-23 10:29:04 -0300

    Added Network EO dummy implementation (#54)
    * Added Network EO dummy implementation and definition to options.json

---
+++ src/gxcore/sd/network.ts
@@ -0,0 +1,61 @@
+import { ConfigurationState } from "../../config/configurationState";
+
+export class GeneXusSDNetwork {
+  /**
+   * Returns the application's services URL
+   */
+  static get applicationServerURL(): string {
+    let config = ConfigurationState.getInstance();
+    let hostName = config.getDynStoredValue("SERVICE_HOSTNAME");
+    let basePath = config.getDynStoredValue("SERVICE_BASE_PATH");
+
+    if (hostName !== undefined) {
+      return hostName + basePath;
+    } else {
+      return location.protocol.concat("//").concat(window.location.hostname);
+    }
+  }
+
+  /**
+   * Returns True if the device can access the specified server; otherwise
+   * False.
+   * Current implementation returns `navigator.onLine`
+   * @param url The URL to check access to, or `undefined` to check
+   * connection to the application's server URL
+   */
+  static isServerAvailable(url?: string): boolean {
+    return navigator.onLine;
+  }
+
+  /**
+   * Gets the connection type the device has to a specific server.
+   * Current implementation returns always `1`, meaning wifi connection.
+   * @param url The URL to check the connection type, or `undefined` to check
+   * connection to the application's server URL
+   */
+  static type(url?: string): number {
+    return 1;
+  }
+
+  /**
+   * Returns true or false whether the connection between the device and the
+   * server could cause a monetary cost to the device user.
+   * Current implementation returns always `false`
+   * @param url The URL to check the connection to, or `undefined` to check
+   * connection to the application's server URL
+   */
+  static trafficBasedCost(url?: string): boolean {
+    return false;
+  }
+
+  /**
+   * It allows to dynamically change the URL of the application.
+   * Current implementation does nothing, prints an error to the console.
+   * @param url
+   */
+  static setApplicationServerURL(url: string) {
+    console.log(
+      "GeneXus.SD.Network's setApplicationServerURL is not available"
+    );
+  }
+}
---
+++ src/gxcore/sd/test/network.spec.ts
@@ -0,0 +1,41 @@
+import { GeneXusSDNetwork } from "../network";
+import { ConfigurationState } from "../../../config/configurationState";
+
+describe("Network external object", () => {
+  it("should return the application's server URL", () => {
+    // TODO: load application settings
+    // ConfigurationState.loadApplicationSettings({
+    //     SERVICE_HOSTNAME: "http://localhost/TestAngular/",
+    //     SERVICE_BASE_PATH: "",
+    //   });
+    //expect(GeneXusSDNetwork.applicationServerURL).toBe("http://localhost/TestAngular/");
+  });
+
+  it("should be able to call IsServerAvailable without parameters", () => {
+    GeneXusSDNetwork.isServerAvailable();
+  });
+
+  it("should be able to call IsServerAvailable with one parameter", () => {
+    GeneXusSDNetwork.isServerAvailable("");
+  });
+
+  it("should be able to call Type without parameters", () => {
+    GeneXusSDNetwork.type();
+  });
+
+  it("should be able to call Type with one parameter", () => {
+    GeneXusSDNetwork.type("");
+  });
+
+  it("should be able to call TrafficBasedCost without parameters", () => {
+    GeneXusSDNetwork.trafficBasedCost();
+  });
+
+  it("should be able to call TrafficBasedCost with one parameter", () => {
+    GeneXusSDNetwork.trafficBasedCost("");
+  });
+
+  it("should be able to call SetApplicationServerURL", () => {
+    GeneXusSDNetwork.setApplicationServerURL("");
+  });
+});
