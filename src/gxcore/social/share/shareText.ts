7a3b0ede57b311bac04b8e2f652a0b82fe1fe7a0
Marcos Crispino <mcrispino@genexus.com>
2019-08-19 15:42:34 -0300

    Added ShareText function (#61)
    * Added ShareText function
    Will work on supported browsers only (see https://caniuse.com/#feat=web-share)
    Falls back to a message telling that the browser does not support sharing.
    
    * ShareText now returns a Promise
    
    * Small improvement to ShareText code

---
+++ src/gxcore/social/share/shareText.ts
@@ -0,0 +1,19 @@
+import { msg } from "../../../misc/msg";
+
+/**
+ * @param text
+ * @param url
+ * @param title
+ */
+export const shareText = (
+  text: string,
+  url: string,
+  title: string
+): Promise<void> => {
+  const nav: any = window.navigator;
+  if (nav && nav.share) {
+    return nav.share({ title, url, text });
+  } else {
+    return msg("Share API not available in this browser", "status");
+  }
+};
