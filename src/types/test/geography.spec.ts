de818168f5bbd5f3940307bdb65e6c93b7af84f5
Daniel Mariño <dangmarm@gmail.com>
2020-12-03 16:21:22 -0300

    Refactor tests

--- src/types/test/geography.spec.ts
+++ src/types/test/geography.spec.ts
@@ -1,29 +1,90 @@
 import { Geography } from "../geography";
 
-describe("Test fromString() method", () => {
+const GEO_EMPTY = {
+  geo: "",
+  longitude: 0,
+  latitude: 0,
+  featureType: "",
+  isEmpty: true,
+  isNull: true
+};
+const GEO_POINT_COORD_ZERO = {
+  geo: "0,0",
+  longitude: 0,
+  latitude: 0,
+  featureType: "POINT",
+  isEmpty: false,
+  isNull: false
+};
+const GEO_POINT_WKT_ZERO = {
+  geo: "POINT(0 0)",
+  longitude: 0,
+  latitude: 0,
+  featureType: "POINT",
+  isEmpty: false,
+  isNull: false
+};
+const GEO_POINT_COORD_VALUE = {
+  geo: "-56.163740158081055,-34.92478600243492",
+  longitude: -34.92478600243492,
+  latitude: -56.163740158081055,
+  featureType: "POINT",
+  isEmpty: false,
+  isNull: false
+};
+const GEO_POINT_WKT_VALUE = {
+  geo: "POINT(-34.92478600243492 -56.163740158081055)",
+  longitude: -34.92478600243492,
+  latitude: -56.163740158081055,
+  featureType: "POINT",
+  isEmpty: false,
+  isNull: false
+};
+const GEO_LINE_WKT_VALUE = {
+  geo:
+    "LINESTRING(-56.16090774536133 -34.928797162523516,-56.1650276184082 -34.89494244739731)",
+  longitude: 0,
+  latitude: 0,
+  featureType: "LINE",
+  isEmpty: false,
+  isNull: false
+};
+const GEO_POLYGON_SIMPLE_WKT_VALUE = {
+  geo:
+    "POLYGON((-56.1510479 -34.8936929,-56.1508977 -34.8962536,-56.1475503 -34.8959984,-56.1510479 -34.8936929))",
+  longitude: 0,
+  latitude: 0,
+  featureType: "POLYGON",
+  isEmpty: false,
+  isNull: false
+};
+const GEO_POLYGON_HOLE_WKT_VALUE = {
+  geo:
+    "POLYGON((-56.1510479 -34.8936929,-56.1508977 -34.8962536,-56.1475503 -34.8959984,-56.1510479 -34.8936929),(-56.1536336 -34.8943177,-56.1529899 -34.8952328,-56.1519814 -34.8947929,-56.1527002 -34.8938513,-56.1536336 -34.8943177))",
+  longitude: 0,
+  latitude: 0,
+  featureType: "POLYGON",
+  isEmpty: false,
+  isNull: false
+};
+
+describe("Test fromString() method checking longitude, latitude and featureType properties", () => {
   const testCases: Array<{
     geo: string;
     longitude: number;
     latitude: number;
     featureType: string;
   }> = [
-    { geo: "", longitude: 0, latitude: 0, featureType: "" },
-    { geo: "0,0", longitude: 0, latitude: 0, featureType: "POINT" },
-    {
-      geo: "-56.163740158081055,-34.92478600243492",
-      longitude: -34.92478600243492,
-      latitude: -56.163740158081055,
-      featureType: "POINT"
-    },
+    GEO_EMPTY,
+    GEO_POINT_COORD_ZERO,
+    GEO_POINT_COORD_VALUE,
+    GEO_POINT_WKT_ZERO,
+    GEO_POINT_WKT_VALUE,
+    GEO_LINE_WKT_VALUE,
+    GEO_POLYGON_SIMPLE_WKT_VALUE,
+    GEO_POLYGON_HOLE_WKT_VALUE,
     {
       geo: "  -56.163740158081055   ,   -34.92478600243492  ",
-      longitude: -34.92478600243492,
-      latitude: -56.163740158081055,
-      featureType: "POINT"
-    },
-    { geo: "POINT(0 0)", longitude: 0, latitude: 0, featureType: "POINT" },
-    {
-      geo: "POINT(-34.92478600243492 -56.163740158081055)",
       longitude: -34.92478600243492,
       latitude: -56.163740158081055,
       featureType: "POINT"
@@ -36,13 +97,6 @@ describe("Test fromString() method", () => {
     },
     {
       geo:
-        "LINESTRING(-56.16090774536133 -34.928797162523516,-56.1650276184082 -34.89494244739731)",
-      longitude: 0,
-      latitude: 0,
-      featureType: "LINE"
-    },
-    {
-      geo:
         "LINESTRING  ( -56.16090774536133   -34.928797162523516  , -56.1650276184082 -34.89494244739731  )",
       longitude: 0,
       latitude: 0,
@@ -50,21 +104,7 @@ describe("Test fromString() method", () => {
     },
     {
       geo:
-        "POLYGON((-56.1510479 -34.8936929,-56.1508977 -34.8962536,-56.1475503 -34.8959984,-56.1510479 -34.8936929))",
-      longitude: 0,
-      latitude: 0,
-      featureType: "POLYGON"
-    },
-    {
-      geo:
         "POLYGON  (  (-56.1510479 -34.8936929  ,  -56.1508977 -34.8962536,   -56.1475503 -34.8959984   ,-56.1510479 -34.8936929))",
-      longitude: 0,
-      latitude: 0,
-      featureType: "POLYGON"
-    },
-    {
-      geo:
-        "POLYGON((-56.1510479 -34.8936929,-56.1508977 -34.8962536,-56.1475503 -34.8959984,-56.1510479 -34.8936929),(-56.1536336 -34.8943177,-56.1529899 -34.8952328,-56.1519814 -34.8947929,-56.1527002 -34.8938513,-56.1536336 -34.8943177))",
       longitude: 0,
       latitude: 0,
       featureType: "POLYGON"
@@ -79,7 +119,7 @@ describe("Test fromString() method", () => {
   ];
 
   for (const testCase of testCases) {
-    it(`Value:${testCase.geo} Longitude:${testCase.longitude} Latitude:${testCase.latitude}`, () => {
+    it(`Value:${testCase.geo} Longitude:${testCase.longitude} Latitude:${testCase.latitude} FeatureType:${testCase.featureType}`, () => {
       const geo = Geography.fromString(testCase.geo);
       const longitude = geo.longitude;
       const latitude = geo.latitude;
@@ -90,121 +130,17 @@ describe("Test fromString() method", () => {
       expect(featureType).toBe(testCase.featureType);
     });
   }
-});
-
-describe("Test longitude,latitude properties", () => {
-  const testCases: Array<{
-    geo: string;
-    longitude: number;
-    latitude: number;
-  }> = [
-    { geo: "", longitude: 0, latitude: 0 },
-    { geo: "0,0", longitude: 0, latitude: 0 },
-    {
-      geo: "-56.163740158081055,-34.92478600243492",
-      longitude: -34.92478600243492,
-      latitude: -56.163740158081055
-    },
-    { geo: "POINT(0 0)", longitude: 0, latitude: 0 },
-    {
-      geo: "POINT(-34.92478600243492 -56.163740158081055)",
-      longitude: -34.92478600243492,
-      latitude: -56.163740158081055
-    },
-    {
-      geo:
-        "LINESTRING(-56.16090774536133 -34.928797162523516, -56.1650276184082 -34.89494244739731)",
-      longitude: 0,
-      latitude: 0
-    },
-    {
-      geo:
-        "POLYGON((-56.1510479 -34.8936929,-56.1508977 -34.8962536,-56.1475503 -34.8959984,-56.1510479 -34.8936929))",
-      longitude: 0,
-      latitude: 0
-    }
-  ];
-
-  for (const testCase of testCases) {
-    it(`Value:${testCase.geo} Longitude:${testCase.longitude} Latitude:${testCase.latitude}`, () => {
-      const geo = Geography.fromString(testCase.geo);
-      const longitude = geo.longitude;
-      const latitude = geo.latitude;
-
-      expect(longitude).toBe(testCase.longitude);
-      expect(latitude).toBe(testCase.latitude);
-    });
-  }
-});
-
-describe("Test featureType property", () => {
-  const testCases: Array<{ geo: string; featureType: string }> = [
-    { geo: "", featureType: "" },
-    { geo: "0,0", featureType: "POINT" },
-    { geo: "-56.163740158081055,-34.92478600243492", featureType: "POINT" },
-    { geo: "POINT(0 0)", featureType: "POINT" },
-    {
-      geo: "POINT(-34.92478600243492 -56.163740158081055)",
-      featureType: "POINT"
-    },
-    {
-      geo:
-        "LINESTRING(-56.16090774536133 -34.928797162523516, -56.1650276184082 -34.89494244739731)",
-      featureType: "LINE"
-    },
-    {
-      geo:
-        "POLYGON((-56.1510479 -34.8936929,-56.1508977 -34.8962536,-56.1475503 -34.8959984,-56.1510479 -34.8936929))",
-      featureType: "POLYGON"
-    },
-    {
-      geo:
-        "POLYGON((-56.1510479 -34.8936929,-56.1508977 -34.8962536,-56.1475503 -34.8959984,-56.1510479 -34.8936929),(-56.1536336 -34.8943177,-56.1529899 -34.8952328,-56.1519814 -34.8947929,-56.1527002 -34.8938513,-56.1536336 -34.8943177))",
-      featureType: "POLYGON"
-    }
-  ];
-
-  for (const testCase of testCases) {
-    it(`Value:${testCase.geo} FeatureType:${testCase.featureType}`, () => {
-      const geo = Geography.fromString(testCase.geo);
-      const featureType = geo.featureType;
-
-      expect(featureType).toBe(testCase.featureType);
-    });
-  }
-});
-
-describe("Test srid property", () => {
-  // TODO: not implemented
 });
 
 describe("Test isEmpty(), isNull() methods", () => {
   const testCases: Array<{ geo: string; isEmpty: boolean; isNull: boolean }> = [
-    { geo: "", isEmpty: true, isNull: true },
-    { geo: "0,0", isEmpty: false, isNull: false },
-    {
-      geo: "-56.163740158081055,-34.92478600243492",
-      isEmpty: false,
-      isNull: false
-    },
-    { geo: "POINT(0 0)", isEmpty: false, isNull: false },
-    {
-      geo: "POINT(-34.92478600243492 -56.163740158081055)",
-      isEmpty: false,
-      isNull: false
-    },
-    {
-      geo:
-        "LINESTRING(-56.16090774536133 -34.928797162523516, -56.1650276184082 -34.89494244739731)",
-      isEmpty: false,
-      isNull: false
-    },
-    {
-      geo:
-        "POLYGON((-56.1510479 -34.8936929,-56.1508977 -34.8962536,-56.1475503 -34.8959984,-56.1510479 -34.8936929))",
-      isEmpty: false,
-      isNull: false
-    }
+    GEO_EMPTY,
+    GEO_POINT_COORD_ZERO,
+    GEO_POINT_COORD_VALUE,
+    GEO_POINT_WKT_ZERO,
+    GEO_POINT_WKT_VALUE,
+    GEO_LINE_WKT_VALUE,
+    GEO_POLYGON_SIMPLE_WKT_VALUE
   ];
 
   for (const testCase of testCases) {
@@ -221,17 +157,11 @@ describe("Test isEmpty(), isNull() methods", () => {
 
 describe("Test setEmpty() method", () => {
   const testCases: Array<{ geo: string }> = [
-    { geo: "-56.163740158081055,-34.92478600243492" },
-    { geo: "POINT(0 0)" },
-    { geo: "POINT(-34.92478600243492 -56.163740158081055)" },
-    {
-      geo:
-        "LINESTRING(-56.16090774536133 -34.928797162523516, -56.1650276184082 -34.89494244739731)"
-    },
-    {
-      geo:
-        "POLYGON((-56.1510479 -34.8936929,-56.1508977 -34.8962536,-56.1475503 -34.8959984,-56.1510479 -34.8936929))"
-    }
+    GEO_POINT_COORD_VALUE,
+    GEO_POINT_WKT_ZERO,
+    GEO_POINT_WKT_VALUE,
+    GEO_LINE_WKT_VALUE,
+    GEO_POLYGON_SIMPLE_WKT_VALUE
   ];
 
   for (const testCase of testCases) {
@@ -318,13 +248,13 @@ describe("Test toGeoJson() method", () => {
       geo:
         "POLYGON((-56.1510479 -34.8936929,-56.1508977 -34.8962536,-56.1475503 -34.8959984,-56.1510479 -34.8936929))",
       geoJson:
-        '{"type":"Polygon","coordinates":[[-56.1510479 -34.8936929,-56.1508977 -34.8962536,-56.1475503 -34.8959984,-56.1510479 -34.8936929]]}'
+        '{"type":"Polygon","coordinates":[[[-56.1510479,-34.8936929],[-56.1508977,-34.8962536],[-56.1475503,-34.8959984],[-56.1510479,-34.8936929]]]}'
     },
     {
       geo:
-        "POLYGON((-56.1510479 -34.8936929,-56.1508977 -34.8962536,-56.1475503 -34.8959984,-56.1510479 -34.8936929), (-56.1536336 -34.8943177,-56.1529899 -34.8952328,-56.1519814 -34.8947929,-56.1527002 -34.8938513,-56.1536336 -34.8943177))",
+        "POLYGON((35 10, 45 45, 15 40, 10 20, 35 10),(20 30, 35 35, 30 20, 20 30))",
       geoJson:
-        '{"type":"Polygon","coordinates":[[-56.1510479 -34.8936929,-56.1508977 -34.8962536,-56.1475503 -34.8959984,-56.1510479 -34.8936929],[-56.1536336 -34.8943177,-56.1529899 -34.8952328,-56.1519814 -34.8947929,-56.1527002 -34.8938513,-56.1536336 -34.8943177]]}'
+        '{"type":"Polygon","coordinates":[[[35,10],[45,45],[15,40],[10,20],[35,10]],[[20,30],[35,35],[30,20],[20,30]]]}'
     }
   ];
 
@@ -338,19 +268,13 @@ describe("Test toGeoJson() method", () => {
   }
 });
 
-describe("toGeoPoint toGeoLine toGeoPolygon", () => {
+describe("Test toGeoPoint() toGeoLine() toGeoPolygon() methods", () => {
   const testCases: Array<{ geo: string }> = [
-    { geo: "" },
-    { geo: "-56.163740158081055, -34.92478600243492" },
-    { geo: "POINT(-34.92478600243492 -56.163740158081055)" },
-    {
-      geo:
-        "LINESTRING(-56.16090774536133 -34.928797162523516,-56.1650276184082 -34.89494244739731)"
-    },
-    {
-      geo:
-        "POLYGON((-56.1510479 -34.8936929,-56.1508977 -34.8962536,-56.1475503 -34.8959984,-56.1510479 -34.8936929))"
-    }
+    GEO_EMPTY,
+    GEO_POINT_COORD_VALUE,
+    GEO_POINT_WKT_VALUE,
+    GEO_LINE_WKT_VALUE,
+    GEO_POLYGON_SIMPLE_WKT_VALUE
   ];
 
   for (const testCase of testCases) {
@@ -392,8 +316,4 @@ describe("Test distance() method", () => {
       expect(distance).toBe(testCase.distance);
     });
   }
-});
-
-describe("intersect", () => {
-  // TODO: not implemented
 });
