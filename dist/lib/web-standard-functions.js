4b3375795d049e7d40adb2003bbd97421a2ccf81
Jaime Roman <jaimemanuelroman@gmail.com>
2021-08-16 13:48:01 -0400

    corregido exportacion

--- dist/lib/web-standard-functions.js
+++ dist/lib/web-standard-functions.js
@@ -23,17 +23,17 @@ var __exportStar =
         __createBinding(exports, m, p);
   };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.Varchar_isEmpty =
+exports.Core_str =
   exports.Text_toString =
-  exports.Text_isEmpty =
-  exports.mytest_toString =
   exports.Numeric_toString =
-  exports.Numeric_isEmpty =
-  exports.Misc_isEmpty =
   exports.DateTime_toString =
   exports.Date_toString =
-  exports.Date_isEmpty =
   exports.Boolean_toString =
+  exports.Varchar_isEmpty =
+  exports.Text_isEmpty =
+  exports.Numeric_isEmpty =
+  exports.Misc_isEmpty =
+  exports.Date_isEmpty =
   exports.Boolean_isEmpty =
     void 0;
 __exportStar(require("./gxcore/client/client-information"), exports);
@@ -42,35 +42,95 @@ __exportStar(require("./types/geography"), exports);
 __exportStar(require("./misc/setLanguage"), exports);
 __exportStar(require("./misc/msg"), exports);
 __exportStar(require("./types/guid"), exports);
-exports.Boolean_isEmpty = require("./bool/isEmpty");
-exports.Boolean_toString = require("./bool/toString");
-exports.Date_isEmpty = require("./date/isEmpty");
-exports.Date_toString = require("./date/toString");
-exports.DateTime_toString = require("./datetime/toString");
-exports.Misc_isEmpty = require("./misc/isEmpty");
-exports.Numeric_isEmpty = require("./numeric/isEmpty");
-exports.Numeric_toString = require("./numeric/toString");
-var toString_1 = require("./numeric/toString");
-Object.defineProperty(exports, "mytest_toString", {
-  enumerable: true,
-  get: function () {
-    return toString_1.toString;
-  },
-});
-exports.Text_isEmpty = require("./text/isEmpty");
-exports.Text_toString = require("./text/toString");
 __exportStar(require("./text/format"), exports);
 __exportStar(require("./text/trim"), exports);
 __exportStar(require("./text/toUpper"), exports);
 __exportStar(require("./text/toLower"), exports);
 __exportStar(require("./text/toNumeric"), exports);
-var isEmpty_1 = require("./text/isEmpty");
-Object.defineProperty(exports, "Varchar_isEmpty", {
+__exportStar(require("./gxcore/common/log"), exports);
+__exportStar(require("./pubSub/pubSubscription"), exports);
+var isEmpty_1 = require("./bool/isEmpty");
+Object.defineProperty(exports, "Boolean_isEmpty", {
   enumerable: true,
   get: function () {
     return isEmpty_1.isEmpty;
   },
 });
-__exportStar(require("./gxcore/common/log"), exports);
-__exportStar(require("./pubSub/pubSubscription"), exports);
+var isEmpty_2 = require("./date/isEmpty");
+Object.defineProperty(exports, "Date_isEmpty", {
+  enumerable: true,
+  get: function () {
+    return isEmpty_2.isEmpty;
+  },
+});
+var isEmpty_3 = require("./misc/isEmpty");
+Object.defineProperty(exports, "Misc_isEmpty", {
+  enumerable: true,
+  get: function () {
+    return isEmpty_3.isEmpty;
+  },
+});
+var isEmpty_4 = require("./numeric/isEmpty");
+Object.defineProperty(exports, "Numeric_isEmpty", {
+  enumerable: true,
+  get: function () {
+    return isEmpty_4.isEmpty;
+  },
+});
+var isEmpty_5 = require("./text/isEmpty");
+Object.defineProperty(exports, "Text_isEmpty", {
+  enumerable: true,
+  get: function () {
+    return isEmpty_5.isEmpty;
+  },
+});
+var isEmpty_6 = require("./text/isEmpty");
+Object.defineProperty(exports, "Varchar_isEmpty", {
+  enumerable: true,
+  get: function () {
+    return isEmpty_6.isEmpty;
+  },
+});
+var toString_1 = require("./bool/toString");
+Object.defineProperty(exports, "Boolean_toString", {
+  enumerable: true,
+  get: function () {
+    return toString_1.toString;
+  },
+});
+var toString_2 = require("./date/toString");
+Object.defineProperty(exports, "Date_toString", {
+  enumerable: true,
+  get: function () {
+    return toString_2.toString;
+  },
+});
+var toString_3 = require("./datetime/toString");
+Object.defineProperty(exports, "DateTime_toString", {
+  enumerable: true,
+  get: function () {
+    return toString_3.toString;
+  },
+});
+var toString_4 = require("./numeric/toString");
+Object.defineProperty(exports, "Numeric_toString", {
+  enumerable: true,
+  get: function () {
+    return toString_4.toString;
+  },
+});
+var toString_5 = require("./text/toString");
+Object.defineProperty(exports, "Text_toString", {
+  enumerable: true,
+  get: function () {
+    return toString_5.toString;
+  },
+});
+var str_1 = require("./numeric/str");
+Object.defineProperty(exports, "Core_str", {
+  enumerable: true,
+  get: function () {
+    return str_1.str;
+  },
+});
 //# sourceMappingURL=web-standard-functions.js.map
